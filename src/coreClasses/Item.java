8d70153aec9be74a63c24224c68df81e5edc9567
ProgrammerJ2013 <HickeyJonathan02@gmail.com>
2015-03-19 17:12:45 +0000

    Item Class

---
+++ src/coreClasses/Item.java
@@ -0,0 +1,83 @@
+package coreClasses;
+
+import java.util.Date;
+
+public class Item {
+
+	private final String PRODUCT_ID;
+	private final String ID;
+	private final Date DATE_OF_MANUFACTURE;
+	private final Date DATE_OF_EXPRIRY;
+	
+	private String currentState;
+	private int assignedUserID;
+	private String cubbyID;
+	private int xPlacementPoint;
+	
+	
+	Item(String productID, String id, Date manufactureDate, Date expriryDate)
+	{
+		PRODUCT_ID = productID;
+		ID = id;
+		DATE_OF_MANUFACTURE = manufactureDate;
+		DATE_OF_EXPRIRY = expriryDate;
+		
+		currentState = "";
+		cubbyID = "";
+		xPlacementPoint = 0;
+		
+		
+	}
+
+	public String getCurrentState() {
+		return currentState;
+	}
+
+	public void setCurrentState(String currentState) {
+		this.currentState = currentState;
+	}
+
+	public int getAssignedUserID() {
+		return assignedUserID;
+	}
+
+	public void setAssignedUserID(int assignedUserID) {
+		this.assignedUserID = assignedUserID;
+	}
+
+	public String getCubbyID() {
+		return cubbyID;
+	}
+
+	public void setCubbyID(String cubbyID) {
+		this.cubbyID = cubbyID;
+	}
+
+	public int getxPlacementPoint() {
+		return xPlacementPoint;
+	}
+
+	public void setxPlacementPoint(int xPlacementPoint) {
+		this.xPlacementPoint = xPlacementPoint;
+	}
+
+	public String getPRODUCT_ID() {
+		return PRODUCT_ID;
+	}
+
+
+
+	public String getID() {
+		return ID;
+	}
+
+	public Date getDATE_OF_MANUFACTURE() {
+		return DATE_OF_MANUFACTURE;
+	}
+
+	public Date getDATE_OF_EXPRIRY() {
+		return DATE_OF_EXPRIRY;
+	}
+	
+	
+}
