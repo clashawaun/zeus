c7f8412d96c5d2a446bf831bb8a0974113fd25d9
ProgrammerJ2013 <HickeyJonathan02@gmail.com>
2015-03-31 18:23:12 +0100

    DB interface finished
    gson added.
    Working on DB class now

--- src/database/DataBase.java
+++ src/database/DataBase.java
@@ -11,12 +11,6 @@ import coreClasses.User;
 public class DataBase implements I_DataBase {
 
 	@Override
-	public boolean isValidLoggin(String username, String password) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
 	public User createUser(String firstName, String secondName,
 			String password, int type, String email, String phone) {
 		// TODO Auto-generated method stub
@@ -24,17 +18,9 @@ public class DataBase implements I_DataBase {
 	}
 
 	@Override
-	public Item createItem(String productID, String id, Date manufactureDate,
-			Date expriryDate) {
+	public boolean isValidLoggin(String username, String password) {
 		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Product createProduct(String name, String description, double price,
-			float height, float width, float depth, float basePriority) {
-		// TODO Auto-generated method stub
-		return null;
+		return false;
 	}
 
 	@Override
@@ -51,6 +37,26 @@ public class DataBase implements I_DataBase {
 	}
 
 	@Override
+	public Item addItem(String productID, String id, Date manufactureDate,
+			Date expriryDate) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Product createProduct(String name, String description, double price,
+			float height, float width, float depth, float basePriority) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ArrayList<Order> getOpenOrders() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
 	public ArrayList<Item> getItems(String productID) {
 		// TODO Auto-generated method stub
 		return null;
@@ -61,5 +67,60 @@ public class DataBase implements I_DataBase {
 		// TODO Auto-generated method stub
 		return null;
 	}
+
+	@Override
+	public void updateOrderStatus(String status) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public ArrayList<Item> get_All_Items_For_User(String userID) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ArrayList<Item> get_All_Items_For_All_Users() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getOrderAddress(String orderID) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Order itemBelonngsTo(String itemID) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void assignItemToOrder(String itemID, String orderID) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void assignItemToUser(String itemID, String userID) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateItemStutas(String itemID, String status) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateProductStutas(String productID, String status) {
+		// TODO Auto-generated method stub
+		
+	}
+
 
 }
--- src/database/I_DataBase.java
+++ src/database/I_DataBase.java
@@ -3,25 +3,58 @@ package database;
 import java.util.ArrayList;
 import java.util.Date;
 
+import coreClasses.Cubby;
 import coreClasses.Item;
 import coreClasses.Order;
 import coreClasses.Product;
+import coreClasses.Sector;
+import coreClasses.Shelve;
 import coreClasses.User;
 
 public interface I_DataBase {
 
-	public boolean isValidLoggin(String username, String password);
-	
 	public User createUser(String firstName, String secondName, String password, int type, String email, String phone );
-	
-	public Item createItem(String productID, String id, Date manufactureDate, Date expriryDate);
-	
-	public Product createProduct(String name, String description, double price, float height, float width, float depth, float basePriority );
+	public boolean isValidLoggin(String username, String password);
 
 	public void registerOrder(ArrayList<String> productIds, String shippingAddress);
-	
 	public Order getOrder(String ID);
 	
+	public Item addItem(String productID, String id, Date manufactureDate, Date expriryDate);
+	public Product createProduct(String name, String description, double price, float height, float width, float depth, float basePriority );
+	
+	public ArrayList<Order> getOpenOrders();
 	public ArrayList<Item> getItems(String productID);
 	public ArrayList<Product> getProducts(String productID);
+	
+	public String getOrderAddress(String orderID);
+	public Order itemBelonngsTo(String itemID);
+	
+	public void assignItemToOrder(String itemID, String orderID);
+	public void assignItemToUser(String itemID, String userID);
+	
+	public void updateOrderStatus(String status);
+	public void updateItemStutas(String itemID, String status);
+	public void updateProductStutas(String productID, String status);
+	
+	public void updateItemPriority(String itemID, int Priority );
+	public void updateProductPriority(String productID, int Priority );
+	public void updateOrderPriority(String orderID, int Priority );
+	
+	public Cubby createCubby();
+	public Shelve createShelve();
+	public Sector createSector();
+	
+	public void assignCubbyToShelve(String cubbyID, String shelveID);
+	public void removeCubbyFromShelve(String cubbyID, String shelveID);
+	
+	public void addItemFromCuby(String itemID, String cubbyID);
+	public void removeItemFromCuby(String itemID, String cubbyID);
+	
+	public void deleteItem(String itemID);
+	public void deletePrdoct(String productID);
+	public void deleteOrder(String Order);
+	
+	public ArrayList<Item> get_All_Items_For_User(String userID);
+	public ArrayList<Item> get_All_Items_For_All_Users();
+
 }
